bb8310c0dc9a4833d622353218a83a20edb8755c
Tudor3510 <tudorcioc@gmail.com>
2021-11-19 19:31:53 +0000

    Starting to write the read text ticket

--- main.asm
+++ main.asm
@@ -1,1 +1,20 @@
-.dat
+.data
+    textToRead: .asciz "Hatz\n"
+    
+.text
+
+.globl main
+main:
+
+readText:
+    mov $4, %eax
+    mov $1, %ebx
+    mov $textToPrint, %ecx
+    mov $6, %edx
+    int $0x80
+
+
+finish:
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
