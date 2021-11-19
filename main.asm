727e42c9d07e4b5aa8944f42845988b595b13881
Tudor3510 <tudorcioc@gmail.com>
2021-11-19 19:44:57 +0000

    Making some tests for understanding assembly

--- main.asm
+++ main.asm
@@ -1,18 +1,32 @@
 .data
-    textToRead: .asciz "Hatz\n"
-    
+    textToRead: .space 102
+    test1: .long 4
+    test2: .space 4
 .text
 
 .globl main
 main:
+    movl %esp, %ebp #for correct debugging
 
 readText:
-    mov $4, %eax
-    mov $1, %ebx
-    mov $textToPrint, %ecx
-    mov $6, %edx
+    mov $3, %eax
+    mov $0, %ebx
+    mov $textToRead, %ecx
+    mov $102, %edx
     int $0x80
 
+testulet:
+    mov $test1, %eax
+    addl $1, test1
+    mov %eax, test2
+    
+
+showText:
+    mov $4, %eax
+    mov $1, %ebx
+    mov $textToRead, %ecx
+    mov $102, %edx
+    int $0x80
 
 finish:
     mov $1, %eax
