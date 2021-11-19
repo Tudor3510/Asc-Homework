e7099cd9958d3680a5e70faa9e1752187371f9de
Tudor3510 <tudorcioc@gmail.com>
2021-11-19 21:12:08 +0000

    Working on etichete for preaparint text

--- main.asm
+++ main.asm
@@ -3,12 +3,17 @@
     textToRead: .space 102
     currPosition: .space 4
     
-    letterToProcess: .space 1
+    whereNumberEnds: .byte 57
+    
+    firstLetter: .space 1
+    secondLetter: .space 1
+    thirdLetter: .space 1
 .text
 
 .globl main
 main:
     movl %esp, %ebp #for correct debugging
+    jmp readText
 
 readText:
     mov $3, %eax
@@ -16,18 +21,32 @@ readText:
     mov $textToRead, %ecx
     mov $102, %edx
     int $0x80
+    jmp preparingText
 
 preparingText:
     movl $0, currPosition
+    jmp preparingTextLoop
     
-    cmpl $length, currPosition   #here we can exit from
-    jge processingTextLoop       #our loop when we are at the end
+preparingTextNumber:
+    subb $47, firstLetter
+    
+preparingTextLoop:
+    cmpl $length, currPosition      #here we can exit from
+    jge processingTextLoop          #our loop when we are at the end (when we reach number 100)
     
     mov $textToRead, %edi
     movl currPosition, %ecx
     movl $0, %edx
-    movb (%edi, %ecx, 1), %dl
-    movb %dl, letterToProcess
+    movb (%edi, %ecx, 1), %dl       #the %edi and %ecx registers should remain unchanged when changing our numbers
+    movb %dl, firstLetter
+    
+    incl currPosition
+    
+    cmpb $whereNumberEnds, firstLetter
+    jle preparingTextNumber
+    
+    
+    
     
    # cmpl letterToProcess
     
