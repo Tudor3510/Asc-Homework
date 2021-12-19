e901622da9d80fd1aebdff7ccbf292c6edfdf056
Tudor3510 <tudorcioc@gmail.com>
2021-12-19 20:09:02 +0200

    Working on the C code

--- CQuery.c
+++ CQuery.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int maxNumber, reqLength, array[100], fixedPoint[100], usedNum[100];
-int index, number;
+int maxNumber, reqLength, array[100], fixedPoint[100], usedNum[100], lastNumPosition[100];
+int index, number, tripleMaxNumber;
+int currPosNum, spaceBetween;
 
 int verifyGood(int pos){
+    for (index =1; index <= maxNumber; index++)
+        lastNumPosition[index] = -35;
 
+    for (index =1; index <= pos; index++){
+        currPosNum = array[index];
+        spaceBetween = index - lastNumPosition[currPosNum] - 1;
+
+        if (spaceBetween < reqLength)
+            return 0;
+    }
+    return 1;
 }
 
 
 void backtracking(int pos){
+    if (pos > tripleMaxNumber){
+        for (index=1; index <= tripleMaxNumber; index++)
+            printf("%d ", array[index]);
+        printf("\n");
+        exit(0);
+    }
+
     if (fixedPoint[pos] == 1){
         if (verifyGood(pos) == 0)
             return;
@@ -16,24 +35,22 @@ void backtracking(int pos){
             return;
         usedNum[array[pos]] += 1;
 
-         backtracking(pos + 1);
+        backtracking(pos + 1);
+
+        usedNum[array[pos]] -= 1;
     }else{
 
         for (number=1; number <= maxNumber; number++){
             if (usedNum[number] < 3){
                 array[pos] = number;
-                if (verifyGood(pos) == 1)
+                if (verifyGood(pos) == 1){
+                    usedNum[number] += 1;
                     backtracking(pos + 1);
+                    usedNum[number] -= 1;
+                }
             }
         }
-
-
-
     }
-
-
-    return;
-
 }
 
 
@@ -42,7 +59,9 @@ int main (){
     scanf("%d", &maxNumber);
     scanf("%d", &reqLength);
 
-    for (index=1; index <= (3 * maxNumber); index++){
+    tripleMaxNumber = 3 * maxNumber;
+
+    for (index=1; index <= tripleMaxNumber; index++){
         scanf("%d", &array[index]);
         if (array[index] != 0)
             fixedPoint[index] = 1;
